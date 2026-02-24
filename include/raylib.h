d876cd0b106544c0a0af28f458fdd10c8b2f9a52
kin-fuyuki <jktechsoftwares@gmail.com>
2026-02-24 00:02:38 -0300

    doin main menu. loading menu will come out today!

--- include/raylib.h
+++ include/raylib.h
@@ -502,6 +502,7 @@ typedef struct AudioStream {
 
     unsigned int sampleRate;    // Frequency (samples per second)
     unsigned int sampleSize;    // Bit depth (bits per sample): 8, 16, 32 (24 not supported)
+	
     unsigned int channels;      // Number of channels (1-mono, 2-stereo, ...)
 } AudioStream;
 
